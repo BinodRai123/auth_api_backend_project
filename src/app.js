c7dcf19c50f9e7070e834c7b2cf9d5cab769c893
binod rai <br163406@gmail.com>
2025-09-06 23:59:08 +0545

    auth middleware is created

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const expres = require("express");
 const authRouter = require("./routes/auth.routes");
 const postRouter = require("./routes/post.routes");
 const cookieParser = require("cookie-parser");
+const authMiddleware = require("./middlewares/auth.middleware");
 
 
 
@@ -9,7 +10,7 @@ const server = expres();
 server.use(expres.json());
 server.use(cookieParser());
 server.use("/api/auth",authRouter);
-server.use("/api/post",postRouter)
+server.use("/api/post",authMiddleware ,postRouter)
 
 
 module.exports = server;
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.models");
+
+async function authMiddleware(req,res){
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({
+            message:"unathorized access, please login first"
+        })
+    }
+
+    try {
+        const decode = jwt.verify(token,process.env.JWT_SECRET_KEY);
+        
+        const user = await userModel.findOne({
+            _id:decode.id
+        })
+
+
+    } catch (error) {
+        console.log({
+            message:"invalid token",
+            error
+        })
+    }
+}
+
+module.exports = authMiddleware;
+
